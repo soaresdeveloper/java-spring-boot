65cd65f63dc65ce91703d09054808bb9a4d04047
lucas-soares <lucas3designer@gmail.com>
2017-08-25 21:42:02 -0300

    Criada pagina de inserção de instituições

--- HelloSpringBoot/src/main/java/br/com/treinaweb/springboot/controllers/InstituicoesController.java
+++ HelloSpringBoot/src/main/java/br/com/treinaweb/springboot/controllers/InstituicoesController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,18 +16,31 @@ import br.com.treinaweb.springboot.repositorios.RepositorioInstituicao;
 @Controller
 @RequestMapping("/instituicoes")
 public class InstituicoesController {
-	
+
 	@Autowired
 	private RepositorioInstituicao repositorioInstituicao;
-	
+
 	@GetMapping("/index")
-	public ModelAndView index(){
-		ModelAndView resultado = new ModelAndView("instituicoes/index");
+	public ModelAndView index() {
+		ModelAndView resultado = new ModelAndView("instituicao/index");
 		List<Instituicao> instituicoes = repositorioInstituicao.findAll();
 		if (instituicoes == null) {
 			instituicoes = new ArrayList<Instituicao>();
 		}
 		resultado.addObject("instituicoes", instituicoes);
 		return resultado;
+	}
+
+	@GetMapping("/inserir")
+	public ModelAndView inserir() {
+		ModelAndView resultado = new ModelAndView("instituicao/inserir");
+		resultado.addObject("instituicao", new Instituicao());
+		return resultado;
+	}
+
+	@PostMapping("/inserir")
+	public String inserir(Instituicao instituicao) {
+		repositorioInstituicao.save(instituicao);
+		return "redirect:/instituicoes/index";
 	}
 }
