ba1dadb0a6129efbf476edf9f8189397a477107f
lucas-soares <lucas3designer@gmail.com>
2017-08-27 18:08:58 -0300

    Criada pesquisa por nome AJAX

--- HelloSpringBoot/src/main/java/br/com/treinaweb/springboot/controllers/InstituicoesController.java
+++ HelloSpringBoot/src/main/java/br/com/treinaweb/springboot/controllers/InstituicoesController.java
@@ -2,6 +2,7 @@ package br.com.treinaweb.springboot.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.treinaweb.springboot.entidades.Instituicao;
@@ -63,5 +65,14 @@ public class InstituicoesController {
 	public String excluir(@PathVariable Long id) {
 		repositorioInstituicao.delete(id);
 		return "redirect:/instituicoes/index";
+	}
+
+	@GetMapping({ "/pesquisarPorNome/{nome}", "/pesquisarPorNome" })
+	public @ResponseBody List<Instituicao> pesquisarPorNome(@PathVariable Optional<String> nome) {
+		if (nome.isPresent()) {
+			return repositorioInstituicao.findByNomeContaining(nome.get());
+		} else {
+			return repositorioInstituicao.findAll();
+		}
 	}
 }
--- HelloSpringBoot/src/main/java/br/com/treinaweb/springboot/repositorios/RepositorioInstituicao.java
+++ HelloSpringBoot/src/main/java/br/com/treinaweb/springboot/repositorios/RepositorioInstituicao.java
@@ -1,9 +1,13 @@
 package br.com.treinaweb.springboot.repositorios;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.treinaweb.springboot.entidades.Instituicao;
 
 public interface RepositorioInstituicao extends JpaRepository<Instituicao, Long>{
 
+	List<Instituicao> findByNomeContaining(String nome);
+	
 }
